41750e18546a7a1c3b6f651e9ceb012c85b2d796
Sanches-23 <pasechnik23.2002@gmail.com>
2023-06-13 22:25:47 +0300

    1st step

--- src/utils/Footer.js
+++ src/utils/Footer.js
@@ -1,38 +1,32 @@
 import React from 'react';
 
-import Image from "./Image";
+// import Image from "./Image";
 // import logo_footer from "../../styles/img/logo-footer.svg";
 
 const Footer = () => {
     return (
         <footer className="footer">
-            {/*<div className="footer__container">*/}
-            {/*    <div className="footer__img">*/}
-            {/*        /!*<Image image={logo_footer}/>*!/*/}
-            {/*    </div>*/}
-            {/*    <div className="footer__wrapper">*/}
-            {/*        <div className="footer__item footer__menu light-text">*/}
-            {/*            /!*<a href="ready-design.html">Дизайни</a>*!/*/}
-            {/*            /!*<a href="ready-furniture.html">Меблі</a>*!/*/}
-            {/*            /!*<a href="about-us.html">Про нас</a>*!/*/}
-            {/*            /!*<a href="contact.html">Зв’язатись</a>*!/*/}
-            {/*        </div>*/}
-            {/*        <div className="footer__item footer__social light-text">*/}
-            {/*            <h3 className="big-text">Контакти</h3>*/}
-            {/*            <a href="src/components/utils/Footer#!">Viber</a>*/}
-            {/*            <a href="src/components/utils/Footer#!">Facebook</a>*/}
-            {/*            <a href="src/components/utils/Footer#!">Instagram</a>*/}
-            {/*        </div>*/}
-            {/*        <div className="footer__item light-text footer__contact">*/}
-            {/*            <a href="mailto:mukomela.oleg@gmail.com">mukomela.oleg@gmail.com</a>*/}
-            {/*            <div className="footer__tel">*/}
-            {/*                <a href="tel:380678672400">+380678672400</a>*/}
-            {/*                <a href="tel:380973402568">+380973402568</a>*/}
-            {/*            </div>*/}
-            {/*            <p>вул. Трудова 29, Хмельницький</p>*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
+            <div className="footer__wrapper">
+                <div className="footer__item footer__contacts">
+                    <h3 className="">Контакти</h3>
+                    <a href="src/components/utils/Footer#!">Телефон</a>
+                    <a href="src/components/utils/Footer#!">Адреса</a>
+                    <a href="src/components/utils/Footer#!">Email</a>
+                </div>
+                <div className="footer__item footer__schedule">
+                    <h3 className="">Графік</h3>
+                    <p>Lorean ipsun dolor sit aneat, conseacteatur adipisci ealit, sead eaiusnod teanpor incidunt ut laborea eat dolorea nagna aliqua.</p>
+                </div>
+
+                {/*<div className="">*/}
+                {/*    <a href="mailto:mukomela.oleg@gmail.com">mukomela.oleg@gmail.com</a>*/}
+                {/*    <div className="">*/}
+                {/*        <a href="tel:380678672400">+380678672400</a>*/}
+                {/*        <a href="tel:380973402568">+380973402568</a>*/}
+                {/*    </div>*/}
+                {/*    <p>вул. Трудова 29, Хмельницький</p>*/}
+                {/*</div>*/}
+            </div>
         </footer>
     );
 }
--- src/utils/Navbar.js
+++ src/utils/Navbar.js
@@ -6,36 +6,36 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <header className="">
-            <div className="">
-                <div className="">
-                    <a href="/" className="">
-                        {/*<Image image={logo} alt="StudioKuhon"/>*/}
+        <header className="header">
+            <div className="wrapper">
+                <div className="header__logo">
+                    <a href="/" className="header__logo-link">
+
                     </a>
-                    <div className="">
-                        <span></span>
-                    </div>
+
+
+
                     <div className="">
                         <a href="src/components/utils/Navbar#!" className="">
                             {/*<Image image={shopping_bag} alt="shopping"/>*/}
                         </a>
                     </div>
-                    <nav className="header__menu">
-                        <ul className="header__list">
-                            <li>
-                                <a href="/" className="header__link">Головна</a>
-                            </li>
-                            <li>
-                                <a href="/chandeliers" className="">Люстри</a>
-                            </li>
-                            <li>
-                                <a href="/about" className="">Про нас</a>
-                            </li>
-                            <li className="">
-                                <a href="src/components/utils/Navbar#!" className="">Замовлення</a>
-                            </li>
-                        </ul>
-                    </nav>
+                    {/*<nav className="header__menu">*/}
+                    {/*    <ul className="header__list">*/}
+                    {/*        <li>*/}
+                    {/*            <a href="/" className="header__link">Головна</a>*/}
+                    {/*        </li>*/}
+                    {/*        <li>*/}
+                    {/*            <a href="/chandeliers" className="">Люстри</a>*/}
+                    {/*        </li>*/}
+                    {/*        <li>*/}
+                    {/*            <a href="/about" className="">Про нас</a>*/}
+                    {/*        </li>*/}
+                    {/*        <li className="">*/}
+                    {/*            <a href="src/components/utils/Navbar#!" className="">Замовлення</a>*/}
+                    {/*        </li>*/}
+                    {/*    </ul>*/}
+                    {/*</nav>*/}
                 </div>
             </div>
         </header>
