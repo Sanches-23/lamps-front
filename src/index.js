c345f1262177623d1ee847526f7548e2f6e6898c
Sanches-23 <pasechnik23.2002@gmail.com>
2023-06-13 22:53:15 +0300

    2.1st step

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import App from "./App";
 
-import './styles/Reset.css'
-import './styles/Fonts.css'
-import './styles/Navbar.css'
+// import './styles/Reset.css'
+// import './styles/Fonts.css'
+// import './styles/Navbar.css'
 
 import './styles/BaseStyle.css'
 
-import './styles/Footer.css'
+// import './styles/Footer.css'
 
 // import "slick-carousel/slick/slick.css";
 // import "slick-carousel/slick/slick-theme.css";
