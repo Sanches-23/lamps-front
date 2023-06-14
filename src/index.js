2323af2d59651ee8d00c0819e39552b3f415c355
Sanches-23 <pasechnik23.2002@gmail.com>
2023-06-14 17:29:40 +0300

    2.2st step

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import ReactDOM from 'react-dom/client';
 import App from "./App";
 
-// import './styles/Reset.css'
 // import './styles/Fonts.css'
+import './styles/Reset.css'
+import './styles/global.css'
 // import './styles/Navbar.css'
+import './styles/Footer.css'
 
-import './styles/BaseStyle.css'
 
-// import './styles/Footer.css'
+
+
+
+
+// import './styles/HomePage.css'
+
+
+
+// import './styles/BaseStyle.css'
 
 // import "slick-carousel/slick/slick.css";
 // import "slick-carousel/slick/slick-theme.css";
